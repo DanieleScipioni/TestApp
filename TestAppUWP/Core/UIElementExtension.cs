e2faa13a40647da06dba64d592d119249ba272de
Daniele Scipioni <daniele.scipioni@gmail.com>
2017-07-09 09:17:44 +0200

    Ref UIElementExtension

--- TestAppUWP/Core/UIElementExtension.cs
+++ TestAppUWP/Core/UIElementExtension.cs
@@ -1,7 +1,11 @@
 ﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 
 namespace TestAppUWP.Core
@@ -9,26 +13,31 @@ namespace TestAppUWP.Core
     // ReSharper disable once InconsistentNaming
     public static class UIElementExtension
     {
-        public static async Task<BitmapImage> GetBitmapImage(this UIElement uiElement)
+        public static async Task<BitmapImage> ToBitmapImage(this UIElement uiElement)
         {
-            var renderTargetBitmap = new RenderTargetBitmap();
-            await renderTargetBitmap.RenderAsync(uiElement);
-            var bitmapImage = new BitmapImage();
-            using (InMemoryRandomAccessStream stream = await GetBitmapImageStream(uiElement))
+            IBuffer buffer = await uiElement.RenderTargetBitmapBuffer();
+
+            using (InMemoryRandomAccessStream stream = await buffer.ToInMemoryRandomAccessStream())
             {
+                var bitmapImage = new BitmapImage();
                 bitmapImage.SetSource(stream);
+                return bitmapImage;
             }
-            return bitmapImage;
         }
 
-        public static async Task<InMemoryRandomAccessStream> GetBitmapImageStream(this UIElement uiElement)
+        public static async Task<InMemoryRandomAccessStream> ToInMemoryRandomAccessStream(this IBuffer buffer)
+        {
+            var stream = new InMemoryRandomAccessStream();
+            await stream.WriteAsync(buffer);
+            stream.Seek(0);
+            return stream;
+        }
+
+        public static async Task<IBuffer> RenderTargetBitmapBuffer(this UIElement uiElement)
         {
             var renderTargetBitmap = new RenderTargetBitmap();
             await renderTargetBitmap.RenderAsync(uiElement);
-            var stream = new InMemoryRandomAccessStream();
-            await stream.WriteAsync(await renderTargetBitmap.GetPixelsAsync());
-            stream.Seek(0);
-            return stream;
+            return await renderTargetBitmap.GetPixelsAsync();
         }
     }
 }
