bb58d24d7d187177a5fa142136a572e396fe2665
Daniele Scipioni <daniele.scipioni@gmail.com>
2017-07-09 10:50:20 +0200

    Add save UIElement to stream snippet

--- TestAppUWP/Core/UIElementExtension.cs
+++ TestAppUWP/Core/UIElementExtension.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Windows.Graphics.Display;
+using Windows.Graphics.Imaging;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Media.Imaging;
@@ -15,5 +18,22 @@ namespace TestAppUWP.Core
             await renderTargetBitmap.RenderAsync(uiElement);
             return await renderTargetBitmap.GetPixelsAsync();
         }
+
+        public static async Task SaveUiElementToStream(this UIElement uiElement, IRandomAccessStream stream)
+        {
+            DisplayInformation displayInformation = DisplayInformation.GetForCurrentView();
+
+            var renderTargetBitmap = new RenderTargetBitmap();
+            await renderTargetBitmap.RenderAsync(uiElement);
+            IBuffer buffer = await renderTargetBitmap.GetPixelsAsync();
+
+            BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.BmpEncoderId, stream);
+            encoder.SetPixelData(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Straight,
+                (uint)renderTargetBitmap.PixelWidth,
+                (uint)renderTargetBitmap.PixelHeight, displayInformation.LogicalDpi, displayInformation.LogicalDpi,
+                buffer.ToArray());
+            await encoder.FlushAsync();
+        }
+
     }
 }
