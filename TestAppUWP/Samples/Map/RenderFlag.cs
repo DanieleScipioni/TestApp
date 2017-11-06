f2d1ee84643b56176f925a7b5018bb2fb76a6c61
Daniele Scipioni <daniele.scipioni@wuerth-phoenix.com>
2017-11-06 18:18:13 +0100

    Add test dimension and save to file

--- TestAppUWP/Samples/Map/RenderFlag.cs
+++ TestAppUWP/Samples/Map/RenderFlag.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Graphics.Display;
 using Windows.Graphics.Imaging;
+using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Shapes;
+using Buffer = Windows.Storage.Streams.Buffer;
 
 namespace TestAppUWP.Samples.Map
 {
@@ -28,7 +30,6 @@ namespace TestAppUWP.Samples.Map
             new Dictionary<string, RandomAccessStreamReference>();
 
         private bool _disposed;
-        private Path _path;
         private Polygon _polygon1;
         private Polygon _polygon2;
         private Line _line1;
@@ -47,20 +48,19 @@ namespace TestAppUWP.Samples.Map
 
             _renderTargetBitmap = new RenderTargetBitmap();
 
-            _path = new Path();
             _polygon = new Polygon
             {
-                Points = new PointCollection {new Point(0, 0), new Point(35, 13), new Point(0, 26)},
+                Points = new PointCollection { new Point(0, 0), new Point(36, 16), new Point(0, 32) },
                 FillRule = FillRule.EvenOdd,
                 Stroke = new SolidColorBrush(Colors.White),
-                StrokeThickness = 2
+                StrokeThickness = 2.5
             };
 
             //_textBlock = new TextBlock {FontSize = 14, CharacterSpacing = -50};
             //_textBlock = new TextBlock {FontSize = 15, CharacterSpacing = 0 };
-            _textBlock = new TextBlock {FontSize = 14, CharacterSpacing = 0 };
+            _textBlock = new TextBlock { FontSize = 14, CharacterSpacing = -50 };
             SetLeft(_textBlock, 2);
-            SetTop(_textBlock, 2.5);
+            SetTop(_textBlock, 5.5);
 
             Children.Add(_polygon);
             Children.Add(_textBlock);
@@ -74,27 +74,28 @@ namespace TestAppUWP.Samples.Map
         {
             _polygon1 = new Polygon
             {
-                Points = new PointCollection { new Point(0, 0), new Point(35, 13), new Point(0, 26) },
+                Points = new PointCollection { new Point(0, 0), new Point(36, 16), new Point(0, 32) },
                 Stroke = _polygon.Stroke,
                 StrokeThickness = _polygon.StrokeThickness
             };
-            SetTop(_polygon1, 3); SetLeft(_polygon1, 1);
+            SetTop(_polygon1, 4); SetLeft(_polygon1, 1);
 
             _line1 = new Line
             {
-                X1 = 35.5, Y1 = 13,
-                X2 = -1, Y2 = 26.5,
-                StrokeThickness = 1.5, Stroke = new SolidColorBrush(Colors.Gray)
+                X1 = 37, Y1 = 17,
+                X2 = -0.5, Y2 = 33.5,
+                StrokeThickness = 1.5,
+                Stroke = new SolidColorBrush(Colors.Gray)
             };
-            SetTop(_line1, 2); SetLeft(_line1, 1);
+            SetTop(_line1, 2); SetLeft(_line1, 0.5);
 
             _polygon2 = new Polygon
             {
-                Points = new PointCollection { new Point(0, 0), new Point(35, 13), new Point(0, 26) },
+                Points = new PointCollection { new Point(0, 0), new Point(36, 16), new Point(0, 32) },
                 Stroke = _polygon.Stroke,
                 StrokeThickness = _polygon.StrokeThickness
             };
-            SetTop(_polygon2, 1.5); SetLeft(_polygon2, 0.5);
+            SetTop(_polygon2, 2); SetLeft(_polygon2, 0.5);
 
             _line2 = new Line
             {
@@ -102,7 +103,6 @@ namespace TestAppUWP.Samples.Map
                 X2 = _line1.X2, Y2 = _line1.Y2,
                 StrokeThickness = _line1.StrokeThickness
             };
-            SetTop(_line2, 0.5); SetLeft(_line2, 0.5);
         }
 
         private void AddMulti()
@@ -148,8 +148,17 @@ namespace TestAppUWP.Samples.Map
             var inMemoryRandomAccessStream = new InMemoryRandomAccessStream();
             await SaveUiElementToPngStream(inMemoryRandomAccessStream);
             if (_disposed) return null;
-            _inMemoryRandomAccessStreams.Add(inMemoryRandomAccessStream);
 
+            StorageFile storageFile =
+                await ApplicationData.Current.LocalFolder.CreateFileAsync($"{id}.png",
+                    CreationCollisionOption.ReplaceExisting);
+            using (IRandomAccessStream randomAccessStream = await storageFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                await RandomAccessStream.CopyAndCloseAsync(inMemoryRandomAccessStream.GetInputStreamAt(0),
+                    randomAccessStream.GetOutputStreamAt(0));
+            }
+
+            _inMemoryRandomAccessStreams.Add(inMemoryRandomAccessStream);
             return _randomAccessStreamReferenceById[id] =
                 RandomAccessStreamReference.CreateFromStream(inMemoryRandomAccessStream);
         }
