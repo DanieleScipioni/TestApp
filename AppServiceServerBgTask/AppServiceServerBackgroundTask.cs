8657b171f3d2c58af7f8f7861c511c821d019e83
Daniele Scipioni <daniele.scipioni@gmail.com>
2017-11-12 12:51:53 +0100

    Add AppServiceServer and AppServiceServerBgTask

---
+++ AppServiceServerBgTask/AppServiceServerBackgroundTask.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.Background;
+using Windows.Foundation.Collections;
+
+namespace AppServiceServerBgTask
+{
+    public sealed class AppServiceServerBackgroundTask : IBackgroundTask
+    {
+        public void Run(IBackgroundTaskInstance taskInstance)
+        {
+            if (!(taskInstance.TriggerDetails is AppServiceTriggerDetails appServiceTriggerDetails)) return;
+
+            AppServiceConnection appServiceConnection = appServiceTriggerDetails.AppServiceConnection;
+            appServiceConnection.RequestReceived += async (sender, args) =>
+            {
+                AppServiceDeferral appServiceDeferral = args.GetDeferral();
+
+                AppServiceRequest appServiceRequest = args.Request;
+                ValueSet request = appServiceRequest.Message;
+
+                await appServiceRequest.SendResponseAsync(new ValueSet {{"count", request.Count}});
+
+                appServiceDeferral.Complete();
+            };
+
+        }
+    }
+}
