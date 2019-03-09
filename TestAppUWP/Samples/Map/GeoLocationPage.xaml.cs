938093430cc483e2d50d9efa5920cc20f63119d0
Daniele Scipioni <daniele.scipioni@gmail.com>
2019-03-09 19:35:28 +0100

    Add MapLocationFinder.FindLocationsAsync feature

--- TestAppUWP/Samples/Map/GeoLocationPage.xaml.cs
+++ TestAppUWP/Samples/Map/GeoLocationPage.xaml.cs
@@ -1,10 +1,21 @@
-﻿namespace TestAppUWP.Samples.Map
+﻿using Windows.UI.Xaml.Controls;
+
+namespace TestAppUWP.Samples.Map
 {
     public sealed partial class GeoLocationPage
     {
+        private readonly GeoLocationPageViewModel _pageViewModel;
+
         public GeoLocationPage()
         {
             InitializeComponent();
+            _pageViewModel = (GeoLocationPageViewModel) DataContext;
+        }
+
+        private void TextBox_OnTextChanging(TextBox textBox, TextBoxTextChangingEventArgs args)
+        {
+            if (!args.IsContentChanging) return;
+            _pageViewModel.Address = textBox.Text;
         }
     }
 }
