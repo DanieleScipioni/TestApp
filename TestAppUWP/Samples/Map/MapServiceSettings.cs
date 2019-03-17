404e09d5caa297598f3d2a0b958bfbc7c004656d
Daniele Scipioni <daniele.scipioni@gmail.com>
2019-03-17 12:56:34 +0100

    Ref code cleaning

--- TestAppUWP/Samples/Map/MapServiceSettings.cs
+++ TestAppUWP/Samples/Map/MapServiceSettings.cs
@@ -2,10 +2,9 @@
 {
     public class MapServiceSettings
     {
-        public const string TokenUwp1 = "TokenUwp1";
-
-        public const string TokenUwp2 = "TokenUwp2";
-
         public static string SelectedToken = TokenUwp1;
+
+        public const string TokenUwp1 = "TokenUwp1";
+        public const string TokenUwp2 = "TokenUwp2";
     }
 }
