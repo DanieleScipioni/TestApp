a7df38e5fac394c3c3c5f2e24b49dd4750075ab1
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-04-05 19:16:49 +0200

    Created and used Sun class

--- TestAppUWP/Samples/Animations/DropShadowStuff/DropShadowPage.xaml.cs
+++ TestAppUWP/Samples/Animations/DropShadowStuff/DropShadowPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System.Numerics;
-using System.Threading.Tasks;
-using TestAppUWP.Core;
-using Windows.UI;
-using Windows.UI.Composition;
-using Windows.UI.Xaml.Hosting;
+﻿using System.Threading.Tasks;
 
 namespace TestAppUWP.Samples.Animations.DropShadowStuff
 {
@@ -12,40 +7,18 @@ namespace TestAppUWP.Samples.Animations.DropShadowStuff
         public DropShadowPage()
         {
             InitializeComponent();
-
             Loaded += async (sender, args) =>
             {
-                Compositor compositor = ElementCompositionPreview.GetElementVisual(this).Compositor;
-                await DropShadowMethod1(compositor);
+                await DropShadowMethod1();
             };
         }
 
-        private async Task DropShadowMethod1(Compositor compositor)
+        private async Task DropShadowMethod1()
         {
-            // Compare cart animation code with
-            // https://stackoverflow.com/questions/38361081/uwp-composition-apply-opacity-mask-to-top-30px-of-a-listview
-
-            DropShadow dropShadow = compositor.CreateDropShadow();
-            dropShadow.BlurRadius = 10f;
-            dropShadow.Color = Colors.Black;
-            dropShadow.Offset = new Vector3(10f, 10f, 0f);
-            dropShadow.Mask = await CompositionDrawingSurfaceFacade2.GetCompositionSurfaceBrush(Button, compositor);
-            //dropShadow.SourcePolicy = CompositionDropShadowSourcePolicy.InheritFromVisualContent;
-
-            LayerVisual layerVisual = compositor.CreateLayerVisual();
-
-            SpriteVisual spriteVisual = compositor.CreateSpriteVisual();
-            spriteVisual.Shadow = dropShadow;
-
-            ElementCompositionPreview.SetElementChildVisual(BorderExternal, spriteVisual);
-
-            Visual buttonVisual = ElementCompositionPreview.GetElementVisual(Button);
-            Visual borderVisual = ElementCompositionPreview.GetElementVisual(Border);
-
-            Button.SizeChanged += (sender1, args1) =>
-            {
-                spriteVisual.Size = new Vector2((float) Button.ActualWidth, (float) Button.ActualHeight);
-            };
+            var sun = new Sun();
+            await sun.DrawShadow(CentralGrid, CentralGridShadowHost);
+            await sun.DrawShadow(Button1, ButtonsShadowHost);
+            await sun.DrawShadow(Button2, ButtonsShadowHost);
         }
     }
 }
---
+++ TestAppUWP/Samples/Animations/DropShadowStuff/Sun.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections.Generic;
+using System.Numerics;
+using System.Threading.Tasks;
+using TestAppUWP.Core;
+using Windows.Foundation;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Hosting;
+using Windows.UI.Xaml.Media;
+
+namespace TestAppUWP.Samples.Animations.DropShadowStuff
+{
+    public class Sun
+    {
+        private readonly Dictionary<FrameworkElement, SpriteVisual> _visualByUiElement;
+        private readonly Dictionary<FrameworkElement, List<FrameworkElement>> _shadowSourceByShadowhost;
+
+        public Sun()
+        {
+            _visualByUiElement = new Dictionary<FrameworkElement, SpriteVisual>();
+            _shadowSourceByShadowhost = new Dictionary<FrameworkElement, List<FrameworkElement>>();
+        }
+
+        private void ShadowHostOnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var shadowHost = (FrameworkElement) sender;
+            List<FrameworkElement> shadowSources = _shadowSourceByShadowhost[shadowHost];
+            foreach (FrameworkElement shadowSource in shadowSources)
+            {
+                SpriteVisual spriteVisual = _visualByUiElement[shadowSource];
+                spriteVisual.Offset = VisualOffset(shadowSource, shadowHost);
+            }
+        }
+
+        public async Task DrawShadow(FrameworkElement shadowSource, FrameworkElement shadowHost)
+        {
+            Compositor compositor = ElementCompositionPreview.GetElementVisual(shadowHost).Compositor;
+
+            List<FrameworkElement> frameworkElements;
+            ContainerVisual shadowHostContainerVisual;
+            if (_shadowSourceByShadowhost.ContainsKey(shadowHost))
+            {
+                frameworkElements = _shadowSourceByShadowhost[shadowHost];
+                shadowHostContainerVisual = (ContainerVisual) ElementCompositionPreview.GetElementChildVisual(shadowHost);
+            }
+            else
+            {
+                frameworkElements = new List<FrameworkElement>();
+                _shadowSourceByShadowhost.Add(shadowHost, frameworkElements);
+                shadowHostContainerVisual = compositor.CreateContainerVisual();
+                ElementCompositionPreview.SetElementChildVisual(shadowHost, shadowHostContainerVisual);
+                shadowHost.SizeChanged += ShadowHostOnSizeChanged;
+            }
+
+            frameworkElements.Add(shadowSource);
+
+            shadowSource.SizeChanged += ShadowSourceOnSizeChanged;
+
+            DropShadow dropShadow = compositor.CreateDropShadow();
+            dropShadow.BlurRadius = 5;
+            dropShadow.Offset = new Vector3(10, 10, 0);
+            dropShadow.Color = Colors.DarkSlateGray;
+            dropShadow.Mask = await ShadowMask(shadowSource, compositor);
+            dropShadow.SourcePolicy = CompositionDropShadowSourcePolicy.Default;
+
+            SpriteVisual visual = compositor.CreateSpriteVisual();
+            visual.Size = new Vector2((float)shadowSource.ActualWidth, (float)shadowSource.ActualHeight);
+            visual.Shadow = dropShadow;
+            visual.Offset = VisualOffset(shadowSource, shadowHost);
+
+            shadowHostContainerVisual.Children.InsertAtTop(visual);
+
+            _visualByUiElement.Add(shadowSource, visual);
+        }
+
+        private async void ShadowSourceOnSizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            var shadowSource = (FrameworkElement) sender;
+            Compositor compositor = ElementCompositionPreview.GetElementVisual(shadowSource).Compositor;
+            SpriteVisual visual = _visualByUiElement[shadowSource];
+            visual.Size = new Vector2((float)shadowSource.ActualWidth, (float)shadowSource.ActualHeight);
+            var dropShadow = (DropShadow) visual.Shadow;
+            dropShadow.Mask = await ShadowMask(shadowSource, compositor);
+        }
+
+        private async Task<CompositionSurfaceBrush> ShadowMask(UIElement uiElement, Compositor compositor)
+        {
+            CompositionDrawingSurface drawingSurface = await CompositionDrawingSurfaceFacade2.GetCompositionDrawingSurface(uiElement, compositor);
+            return compositor.CreateSurfaceBrush(drawingSurface);
+        }
+
+        private Vector3 VisualOffset(UIElement shadowSource, FrameworkElement shoadowHost)
+        {
+            GeneralTransform transformToVisual = shadowSource.TransformToVisual(shoadowHost);
+            Point transformPoint = transformToVisual.TransformPoint(new Point(0, 0));
+            return new  Vector3((float) transformPoint.X, (float) transformPoint.Y, 0);
+        }
+    }
+}
