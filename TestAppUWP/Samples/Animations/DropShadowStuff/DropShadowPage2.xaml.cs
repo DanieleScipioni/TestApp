2ccd3366a01b8684615e6e13d1a6f46964491d59
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-04-07 14:02:01 +0200

    Add DropShadowPage2

---
+++ TestAppUWP/Samples/Animations/DropShadowStuff/DropShadowPage2.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TestAppUWP.Samples.Animations.DropShadowStuff
+{
+    public sealed partial class DropShadowPage2
+    {
+        public DropShadowPage2()
+        {
+            InitializeComponent();
+        }
+    }
+}
