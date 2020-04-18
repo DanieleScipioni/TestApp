d25ec86dbc303c362227afc0b3535e6e6ad5a16d
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-04-18 10:33:03 +0200

    Ref StretchPathIcon with OnApplyTemplate

--- TestAppUWP.AppShell/Samples/PathButton/StretchPathIcon.cs
+++ TestAppUWP.AppShell/Samples/PathButton/StretchPathIcon.cs
@@ -3,29 +3,25 @@ using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Shapes;
 
-namespace TestAppUWP.Samples.PathButton
+namespace TestAppUWP.AppShell.Samples.PathButton
 {
-    public class StretchPathIcon : Windows.UI.Xaml.Controls.PathIcon
+    public class StretchPathIcon : PathIcon
     {
         private Path _path;
 
-        public StretchPathIcon()
+        protected override void OnApplyTemplate()
         {
-            Loaded += PathIconStr_Loaded;
-        }
-
-        private void PathIconStr_Loaded(object sender, RoutedEventArgs e)
-        {
+            base.OnApplyTemplate();
             var grid = (Grid)VisualTreeHelper.GetChild(this, 0);
-            _path = (Path) grid.Children[0];
+            _path = (Path)grid.Children[0];
             _path.Stretch = Stretch;
         }
 
-        #region DependencyProperties
+        #region StretchProperty
 
         public static readonly DependencyProperty StretchProperty = DependencyProperty.Register(
-            nameof(Stretch), typeof(Stretch), typeof(StretchPathIcon), new PropertyMetadata(default(Stretch), StretchPropertyChangedCallback));
-
+            nameof(Stretch), typeof(Stretch), typeof(StretchPathIcon),
+            new PropertyMetadata(default(Stretch), StretchPropertyChangedCallback));
 
         private static void StretchPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
