3e046e2bfd1978e397c646e504efc590456670ae
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-06-06 23:30:44 +0200

    temp dev

--- TestAppUWP.AppShell/Samples/Test/TestPage.xaml.cs
+++ TestAppUWP.AppShell/Samples/Test/TestPage.xaml.cs
@@ -1,10 +1,23 @@
-﻿namespace TestAppUWP.AppShell.Samples.Test
+﻿using System;
+using System.Threading;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml.Input;
+
+namespace TestAppUWP.AppShell.Samples.Test
 {
     public sealed partial class TestPage
     {
         public TestPage()
         {
             InitializeComponent();
+        }
+
+        private void UIElement_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            ThreadPool.QueueUserWorkItem(async state =>
+            {
+                AppRestartFailureReason appRestartFailureReason = await CoreApplication.RequestRestartAsync("");
+            });
         }
     }
 }
