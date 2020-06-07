4dae681fb0453fb5dc3e1331017be1c4e7870602
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-06-07 10:39:23 +0200

    Add some toolkitControls:TabViewItem

--- TestAppUWP.AppShell/Samples/Test/TestPage.xaml.cs
+++ TestAppUWP.AppShell/Samples/Test/TestPage.xaml.cs
@@ -1,10 +1,42 @@
-﻿namespace TestAppUWP.AppShell.Samples.Test
+﻿using System.Collections.Generic;
+
+namespace TestAppUWP.AppShell.Samples.Test
 {
     public sealed partial class TestPage
     {
+        private List<Book> listItem;
+
         public TestPage()
         {
+            listItem = new List<Book>
+            {
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"}
+            };
             InitializeComponent();
         }
+    }
+
+    internal class Book
+    {
+        public string Title;
     }
 }
