b9f3e58ea53df39acf1db4e01a587e0cac9e65d5
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-06-07 12:01:30 +0200

    Add Test/ScrollBar

---
+++ TestAppUWP.AppShell/Samples/Test/ScrollBar.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using Windows.ApplicationModel.Core;
+using Windows.UI.Xaml.Input;
+
+namespace TestAppUWP.AppShell.Samples.Test
+{
+    public sealed partial class ScrollBar
+    {
+        public ScrollBar()
+        {
+            InitializeComponent();
+        }
+    }
+}
