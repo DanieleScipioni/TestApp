54701bd283adb4550534600c516bc259660d7f24
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-06-07 16:46:02 +0200

    Add RequestRestartAsync

--- TestAppUWP.AppShell/Samples/RootNavigation/RootNavigationPage.xaml.cs
+++ TestAppUWP.AppShell/Samples/RootNavigation/RootNavigationPage.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
 using Windows.Storage;
 using Windows.System;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
+using TestAppUWP.Logic.Logs;
 
 namespace TestAppUWP.AppShell.Samples.RootNavigation
 {
@@ -29,6 +34,30 @@ namespace TestAppUWP.AppShell.Samples.RootNavigation
         private async void OpenLocalState_OnClick(object sender, RoutedEventArgs e)
         {
             await Launcher.LaunchFolderAsync(ApplicationData.Current.LocalFolder);
+        }
+
+        private async void RequestRestartAsync_OnClick(object sender, RoutedEventArgs e)
+        {
+            Logger logger = LogFactory.GetLogger(nameof(RequestRestartAsync_OnClick));
+
+            await Task.Delay(5000);
+            await Dispatcher.RunAsync(CoreDispatcherPriority.Normal,
+                async () => {
+                    try
+                    {
+                        AppRestartFailureReason arfr = await CoreApplication.RequestRestartAsync("");
+                        if (arfr == AppRestartFailureReason.NotInForeground || arfr == AppRestartFailureReason.Other)
+                        {
+                            logger.Log($"AppRestartFailureReason {arfr}");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        logger.Log($"AppRestartFailureReason Exception {ex.Message}");
+                    }
+                    logger.Flush();
+                });
+            ;
         }
     }
 }
