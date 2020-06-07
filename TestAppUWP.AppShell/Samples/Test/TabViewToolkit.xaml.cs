d3c7315fd16cc02549425562548b74f968af94ba
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-06-07 10:49:40 +0200

    Add TestPage with Pivot

---
+++ TestAppUWP.AppShell/Samples/Test/TabViewToolkit.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace TestAppUWP.AppShell.Samples.Test
+{
+    public sealed partial class TabViewToolkit
+    {
+        private List<Book> listItem;
+
+        public TabViewToolkit()
+        {
+            listItem = new List<Book>
+            {
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"},
+                new Book {Title = "Test string"}
+            };
+            InitializeComponent();
+        }
+    }
+}
--- TestAppUWP.AppShell/Samples/Test/TestPage.xaml.cs
+++ TestAppUWP.AppShell/Samples/Test/TestPage.xaml.cs
@@ -1,36 +1,9 @@
-﻿using System.Collections.Generic;
-
-namespace TestAppUWP.AppShell.Samples.Test
+﻿namespace TestAppUWP.AppShell.Samples.Test
 {
     public sealed partial class TestPage
     {
-        private List<Book> listItem;
-
         public TestPage()
         {
-            listItem = new List<Book>
-            {
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"},
-                new Book {Title = "Test string"}
-            };
             InitializeComponent();
         }
     }
