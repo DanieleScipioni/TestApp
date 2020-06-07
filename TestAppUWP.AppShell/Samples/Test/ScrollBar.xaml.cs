aaee16234d57cd76bd4034dac10eb2220d4010c3
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-06-07 16:38:15 +0200

    Add classic ScrollBar style

--- TestAppUWP.AppShell/Samples/Test/ScrollBar.xaml.cs
+++ TestAppUWP.AppShell/Samples/Test/ScrollBar.xaml.cs
@@ -1,10 +1,46 @@
-﻿namespace TestAppUWP.AppShell.Samples.Test
+﻿using System.Collections.Generic;
+
+namespace TestAppUWP.AppShell.Samples.Test
 {
     public sealed partial class ScrollBar
     {
+        private List<string> _items;
+
         public ScrollBar()
         {
             InitializeComponent();
+            _items = new List<string>
+            {
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text",
+                "sample text"
+            };
         }
     }
 }
