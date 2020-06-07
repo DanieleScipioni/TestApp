153fb40f167f54f0176aa3e4d9e386a2949754af
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-06-07 11:53:37 +0200

    Add load last PivotItem

--- TestAppUWP.AppShell/Samples/Test/TestPage.xaml.cs
+++ TestAppUWP.AppShell/Samples/Test/TestPage.xaml.cs
@@ -1,10 +1,50 @@
-﻿namespace TestAppUWP.AppShell.Samples.Test
+﻿using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.UI.Xaml;
+
+namespace TestAppUWP.AppShell.Samples.Test
 {
     public sealed partial class TestPage
     {
+        private const string PivotSelectedindexKey = "Pivot.SelectedIndex";
+
         public TestPage()
         {
             InitializeComponent();
+            Loaded += OnLoaded;
+            Unloaded += OnUnloaded;
+        }
+
+        private void OnUnloaded(object sender, RoutedEventArgs args)
+        {
+            Application.Current.Suspending -= OnSuspending;
+            SavePageState();
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs args)
+        {
+            if (ApplicationData.Current.LocalSettings.Containers.TryGetValue(nameof(TestPage), out ApplicationDataContainer container))
+            {
+                if (container.Values.TryGetValue(PivotSelectedindexKey, out object value) && value is int selectedIndex)
+                {
+                    // ReSharper disable once PossibleNullReferenceException
+                    Pivot.SelectedIndex = selectedIndex < Pivot.Items.Count ? selectedIndex : Pivot.Items.Count - 1;
+                }
+            }
+
+            Application.Current.Suspending += OnSuspending;
+        }
+
+        private void OnSuspending(object sender, SuspendingEventArgs args)
+        {
+            SavePageState();
+        }
+
+        private void SavePageState()
+        {
+            ApplicationDataContainer dataContainer = ApplicationData.Current.LocalSettings.CreateContainer(
+                nameof(TestPage), ApplicationDataCreateDisposition.Always);
+            dataContainer.Values[PivotSelectedindexKey] = Pivot.SelectedIndex;
         }
     }
 
