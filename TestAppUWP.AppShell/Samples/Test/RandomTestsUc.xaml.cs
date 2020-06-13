4c9ca0147629b5ac4684d3c3a0dc2f5223699e03
Daniele Scipioni <daniele.scipioni@gmail.com>
2020-06-13 21:08:39 +0200

    Add Random Tests PivotItem to TestPage

---
+++ TestAppUWP.AppShell/Samples/Test/RandomTestsUc.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Windows.UI.Xaml;
+
+namespace TestAppUWP.AppShell.Samples.Test
+{
+    public sealed partial class RandomTestsUc
+    {
+        public RandomTestsUc()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text), typeof(string), typeof(RandomTestsUc),
+            new PropertyMetadata(default(string), (o, args) =>
+            {
+                var randomTestsUc = (RandomTestsUc) o;
+                randomTestsUc.TextBlock.Text = (string) args.NewValue;
+            }));
+
+        public string Text
+        {
+            get => (string) GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+    }
+}
